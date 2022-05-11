7043707c20a04cdf719ac7f27ba3c597808f1a62
Matheus Grativol <matheus_grati@hotmail.com>
2022-05-11 14:06:34 -0300

    interação com o localStorage

--- clinica-matheus/src/Pages/FormPage/FormPage.js
+++ clinica-matheus/src/Pages/FormPage/FormPage.js
@@ -1,33 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { Base_URL } from "../../Components/Base_URL/Base_URL";
 import { Form, H2Form, InputForm, InputFormCpf, SelectForm } from "./styled";
 import Button from '@mui/material/Button';
 
 
-const ListPage = () =>{
+const ListPage = () => {
 
-    const [sources , setSources] = useState([])
-    const [sourceId , setSourceId] = useState("")
-    const [nameUser , setNameUser] = useState("")
-    const [birthdateUser , setBirthdateUser] = useState("")
-    const [cpfUser , setCpfUser] = useState("")
+    const [sources, setSources] = useState([])
+    const [sourceId, setSourceId] = useState("")
+    const [nameUser, setNameUser] = useState("")
+    const [birthdateUser, setBirthdateUser] = useState("")
+    const [cpfUser, setCpfUser] = useState("")
     // const [formStorage , setFormStorage] = useState([])
 
-    const history = useHistory()
-
     let id = useParams()
-    
-    const goToBack = () =>{
-        history.goBack()
-    }
 
-    useEffect(()=>{
+    useEffect(() => {
         getSources()
     }, [])
-    
+
     const changeSource = event => {
         setSourceId(event.target.value)
     };
@@ -54,66 +47,71 @@ const ListPage = () =>{
             })
     }
 
-    const formUser = {
-        specialty_id : id.specialty_id,
-        professional_id : id.professional_id,
-        name : nameUser,
-        cpf : cpfUser,
-        source_id : sourceId,
-        birthdate : birthdateUser,
-        date_time : new Date().toISOString()
+    
+
+    const getStorage = () => {
+        return JSON.parse(localStorage.getItem("dado")) || []
     }
 
-    const saveForm = (event)=>{
-        // event.preventDefault();
-        // formStorage.push(FormUser)
-        localStorage.setItem("dado",`${JSON.stringify(formUser)}`)
+    const saveForm = (event) => {
+        event.preventDefault()
+        const formUser = {
+            specialty_id: id.specialty_id,
+            professional_id: id.professional_id,
+            name: nameUser,
+            cpf: cpfUser,
+            source_id: sourceId,
+            birthdate: birthdateUser,
+            date_time: new Date().toISOString()
+        }
+        if(!formUser.name || !formUser.cpf || !formUser.source_id || !formUser.birthdate){
+            return alert("Preencha os campos corretamente")
+        }
+        const storages = getStorage()
+        storages.push(formUser)
+        localStorage.setItem("dado", `${JSON.stringify(storages)}`)
     }
 
-    console.log(formUser)
-    return(
+    return (
         <div>
-            <div>
-                <button onClick={goToBack}>Voltar</button>
-            </div>
             <div>
                 <H2Form>Preencha seus dados</H2Form>
                 <Form onSubmit={saveForm}>
-                    <InputForm 
-                        onChange={changeName} 
+                    <InputForm
+                        onChange={changeName}
                         value={nameUser}
-                        placeholder="Nome completo" 
+                        placeholder="Nome completo"
                         type={"text"}
-                        pattern={"^.{3,}"}
-                        title={"O nome deve ter no mínimo 3 letras"}
                     />
-                    <SelectForm onChange={changeSource}>
+                    <SelectForm onChange={changeSource} required>
                         <option value={""}>Como conheceu?</option>
                         {sources.map((source) => {
-                        return (
-                            <option key={source.origem_id} value={source.origem_id}>
-                                {source.nome_origem}
-                            </option>
-                        )
-                    })}
+                            return (
+                                <option key={source.origem_id} value={source.origem_id}>
+                                    {source.nome_origem}
+                                </option>
+                            )
+                        })}
                     </SelectForm>
-                    <InputForm 
+                    <InputForm
                         onChange={changeBirthdate}
                         value={birthdateUser}
-                        placeholder="Nascimento" 
+                        placeholder="Nascimento"
                         type={"date"}
                     />
-                    <InputFormCpf 
+                    <InputFormCpf
                         onChange={changeCpf}
                         value={cpfUser}
-                        placeholder="CPF" 
+                        placeholder="CPF"
                         type={"number"}
                     />
-                    <Button onClick={saveForm()} variant="contained" color="success">SOLICITAR HORÁRIOS</Button>
+                    <Button onClick={saveForm} variant="contained" color="success">SOLICITAR HORÁRIOS</Button>
                 </Form>
             </div>
 
-            <h1>ListPage</h1>
+            <div>
+
+            </div>
         </div>
     )
 }
